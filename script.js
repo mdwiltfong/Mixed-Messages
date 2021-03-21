adb97407efd68e24b1ce7f9653836917d2e54fd3
mdwiltfong <wiltfong.michael1@gmail.com>
2021-03-20 22:07:25 -0400

    changes to main.html and script.js
    I've created an input tag that will initiate the function zodiacSigns. Though the function is correctly calling upon the user's inputed birthday, i still need to make the function divide the inputed birthday correctly.

--- script.js
+++ script.js
@@ -61,17 +61,14 @@ var zodiacs = {
     }
 }
 
+function zodiacSigns(){
+var x=document.getElementById("birthday").value;
+let arr=x.split("");
+let date=[];
+date.forEach(n => {
+    
+});
 
+}
 
-(function(){ 
-    var form = document.getElementById('login');       // Get form element
-  
-    addEvent(form, 'submit', function(e) {             // When user submits form
-      e.preventDefault();                              // Stop it being sent
-      var elements = this.elements;                    // Get all form elements
-      var username = elements.username.value;          // Select username entered
-      var msg      = 'Welcome ' + username;            // Create welcome message
-      document.getElementById('main').textContent = msg; // Write welcome message
-    });
-  }());
-
+zodiacSigns()
